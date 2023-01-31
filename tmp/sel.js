d67b0789f5f75861ade48695c1c2385f26ac013c
Yuhao Zhu <yzhu@utexas.edu>
2023-01-31 16:24:26 -0500

    update sel.

--- tmp/sel.js
+++ tmp/sel.js
@@ -14,11 +14,31 @@ var oRedColor = 'rgba(218, 37, 0, 0.3)';
 var oGreenColor = 'rgba(0, 143, 0, 0.3)';
 var oBlueColor = 'rgba(1, 25, 147, 0.5)';
 
-var p_norm = [0.9795006397, -0.2013704401, 0.005333160206];
-var d_norm = [-0.8959739281, 0.4425391762, -0.03727999099];
-var t_norm = [0.1428342021, -0.1413451732, 0.9796019256];
-var norm = p_norm;
+// vectors for confusion lines
+var p_line = [0.9795006397, -0.2013704401, 0.005333160206];
+var d_line = [-0.8959739281, 0.4425391762, -0.03727999099];
+var t_line = [0.1428342021, -0.1413451732, 0.9796019256];
+var line = p_line;
+
 var color1, color2, color3;
+var sim = false;
+var type = 0; // 0 for P, 1 for D, 2 for T
+
+// normals of the projection planes
+var p_norm1 = [-1.9082658681515485, 2.117751626087778, -0.20939929937650947];
+var p_norm2 = [-1.8983441434416113, 2.04613441386516, -0.14772500661017962];
+var d_norm1 = p_norm1;
+var d_norm2 = d_norm2;
+var t_norm1 = [-0.8559262314423839, 1.1229313663856852, -0.26690712337317146];
+var t_norm2 = [-0.07331466590406314, 0.22723495600946306, -0.15386703720552092];
+var norm = p_norm1;
+
+var hpe_xyz2lms = [[0.3897,0.689,-0.0787], [-0.2298,1.1834,0.0464], [0,0,1]];
+var hpe_xyz2lms_d65 = [[0.40,0.71,-0.08], [-0.23,1.17,0.05], [0.00,0.00,0.92]]; // D65 adapted
+var RGB2xyz = math.transpose([[0.4123151515,0.21,0.01932727273], [0.3576,0.72,0.1192], [0.1805,0.07,0.9506333333]]);
+var RGB2lms = math.multiply(hpe_xyz2lms, RGB2xyz);
+var lms2RGB = math.inv(RGB2lms);
+
 
 // https://docs.mathjax.org/en/v2.1-latest/typeset.html
 var QUEUE = MathJax.Hub.queue; // shorthand for the queue
@@ -82,6 +102,37 @@ function sRGB2Name(color) {
   return n_name;
 }
 
+function prot_project(normal, m, s) {
+  return - (normal[1] * m + normal[2] * s) / normal[0];
+};
+
+function deut_project(normal, l, s) {
+  return - (normal[1] * l + normal[2] * s) / normal[0];
+};
+
+function trit_project(normal, l, m) {
+  return - (normal[1] * l + normal[2] * m) / normal[0];
+};
+
+function project(colors_LMS) {
+  if (type == 0) {
+    return [[prot_project(norm, colors_LMS[1][0], colors_LMS[2][0]), colors_LMS[1][0], colors_LMS[2][0]],
+            [prot_project(norm, colors_LMS[1][1], colors_LMS[2][1]), colors_LMS[1][1], colors_LMS[2][1]],
+            [prot_project(norm, colors_LMS[1][2], colors_LMS[2][2]), colors_LMS[1][2], colors_LMS[2][2]]
+    ];
+  } else if (type == 1) {
+    return [[colors_LMS[0][0], deut_project(norm, colors_LMS[0][0], colors_LMS[2][0]), colors_LMS[2][0]],
+            [colors_LMS[0][1], deut_project(norm, colors_LMS[0][1], colors_LMS[2][1]), colors_LMS[2][1]],
+            [colors_LMS[0][2], deut_project(norm, colors_LMS[0][2], colors_LMS[2][2]), colors_LMS[2][2]]
+    ];
+  } else {
+    return [[colors_LMS[0][0], colors_LMS[1][0], deut_project(norm, colors_LMS[0][0], colors_LMS[1][0])],
+            [colors_LMS[0][1], colors_LMS[1][1], deut_project(norm, colors_LMS[0][1], colors_LMS[1][1])],
+            [colors_LMS[0][2], colors_LMS[1][2], deut_project(norm, colors_LMS[0][2], colors_LMS[1][2])]
+    ];
+  }
+}
+
 function plotRGB(plotId) {
   var allPoints = math.transpose([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 0], [1, 0, 1], [0, 1, 1], [1, 1, 1]]);
 
@@ -134,6 +185,32 @@ function plotRGB(plotId) {
     text: [0, 0, 0],
     type: 'scatter3d',
     marker: {
+      size: 8,
+      opacity: 1,
+      color: [0,0,0],
+    },
+    line: {
+      width: 1,
+      color: '#000000',
+    },
+    //mode: 'markers',
+    showlegend: true,
+    name: 'actual colors',
+    opacity:0.8,
+    hovertemplate: 'Actual: %{text}<br>' +
+      '<br>R: %{x}' +
+      '<br>G: %{y}' +
+      '<br>B: %{z}<extra></extra>',
+  };
+  traces.push(line);
+
+  var sim_line = {
+    x: [0],
+    y: [0],
+    z: [0],
+    text: [0, 0, 0],
+    type: 'scatter3d',
+    marker: {
       size: 5,
       opacity: 1,
       color: [0,0,0],
@@ -144,14 +221,14 @@ function plotRGB(plotId) {
     },
     //mode: 'markers',
     showlegend: true,
-    //name: i,
+    name: 'simulation',
     opacity:0.8,
-    hovertemplate: '%{text}<br>' +
+    hovertemplate: 'Simulation: %{text}<br>' +
       '<br>R: %{x}' +
       '<br>G: %{y}' +
       '<br>B: %{z}<extra></extra>',
   };
-  traces.push(line);
+  traces.push(sim_line);
 
   var data = traces;
 
@@ -163,7 +240,7 @@ function plotRGB(plotId) {
       b: 0,
       t: 0
     },
-    showlegend: false,
+    showlegend: true,
     legend: {
       x: 0.1,
       xanchor: 'left',
@@ -232,40 +309,63 @@ function updatePlot(theta, plotId) {
   var u = 1/Math.sqrt(3)
   var cos = Math.cos(theta)
   var sin = Math.sin(theta)
-  
+
   var rotMat = [
     [cos + u*u*(1-cos), u*u*(1-cos)-u*sin, u*u*(1-cos)+u*sin],
     [u*u*(1-cos)+u*sin, cos+u*u*(1-cos), u*u*(1-cos)-u*sin],
     [u*u*(1-cos)-u*sin, u*u*(1-cos)+u*sin, cos+u*u*(1-cos)]
   ];
 
-  var newPoints = math.multiply(rotMat, math.transpose([color1, color2, color3]));
-  newColors = [RGB2sRGB([newPoints[0][0], newPoints[1][0], newPoints[2][0]]),
-               RGB2sRGB([newPoints[0][1], newPoints[1][1], newPoints[2][1]]),
-               RGB2sRGB([newPoints[0][2], newPoints[1][2], newPoints[2][2]])
-              ];
-  newTexts = [RGB2sRGB([newPoints[0][0], newPoints[1][0], newPoints[2][0]]),
-              RGB2sRGB([newPoints[0][1], newPoints[1][1], newPoints[2][1]]),
-              RGB2sRGB([newPoints[0][2], newPoints[1][2], newPoints[2][2]])
-             ];
+  // Convention: in |Points| each color is a column and in |Colors| each color is a row
+  var rotPoints_RGB = math.multiply(rotMat, math.transpose([color1, color2, color3]));
+  rotColors_sRGB = [RGB2sRGB([rotPoints_RGB[0][0], rotPoints_RGB[1][0], rotPoints_RGB[2][0]]),
+                    RGB2sRGB([rotPoints_RGB[0][1], rotPoints_RGB[1][1], rotPoints_RGB[2][1]]),
+                    RGB2sRGB([rotPoints_RGB[0][2], rotPoints_RGB[1][2], rotPoints_RGB[2][2]])
+                   ];
 
-  var data_update = {'x': [newPoints[0]], 'y': [newPoints[1]], 'z': [newPoints[2]],
-                     'marker.color': [newColors], 'text': [newTexts]};
+  // update actual colors in the 3D plot
+  var data_update = {'x': [rotPoints_RGB[0]], 'y': [rotPoints_RGB[1]], 'z': [rotPoints_RGB[2]],
+                     'marker.color': [rotColors_sRGB], 'text': [rotColors_sRGB]};
 
   var plot = document.getElementById(plotId);
   Plotly.update(plot, data_update, {}, [12]);
 
+  // update simulated colors in the 3D plot
+  var rotPoints_LMS = math.multiply(RGB2lms, rotPoints_RGB);
+  var simColors_LMS = project(rotPoints_LMS);
+  var simPoints_RGB = math.multiply(lms2RGB, math.transpose(simColors_LMS));
+  var simColors_RGB = math.transpose(simPoints_RGB);
+  var simColors_sRGB = [RGB2sRGB(simColors_RGB[0]),
+                        RGB2sRGB(simColors_RGB[1]),
+                        RGB2sRGB(simColors_RGB[2])
+                       ];
+
+  var data_update = {'x': [simPoints_RGB[0]], 'y': [simPoints_RGB[1]], 'z': [simPoints_RGB[2]],
+                     'marker.color': [simColors_sRGB], 'text': [simColors_sRGB]};
+  Plotly.update(plot, data_update, {}, [13]);
+
   // update square colors
-  var newColors = math.transpose(newPoints);
-  $('#h11').text(RGB2sRGB(newColors[0]));
-  $('#h12').text(RGB2sRGB(newColors[1]));
-  $('#h13').text(RGB2sRGB(newColors[2]));
-  $('#n11').text(sRGB2Name(RGB2sRGB(newColors[0])));
-  $('#n12').text(sRGB2Name(RGB2sRGB(newColors[1])));
-  $('#n13').text(sRGB2Name(RGB2sRGB(newColors[2])));
-  $('#s11').css('background-color', RGB2sRGB(newColors[0]));
-  $('#s12').css('background-color', RGB2sRGB(newColors[1]));
-  $('#s13').css('background-color', RGB2sRGB(newColors[2]));
+  if (sim) {
+    $('#h11').text('');
+    $('#h12').text('');
+    $('#h13').text('');
+    $('#n11').text('');
+    $('#n12').text('');
+    $('#n13').text('');
+    $('#s11').css('background-color', simColors_sRGB[0]);
+    $('#s12').css('background-color', simColors_sRGB[1]);
+    $('#s13').css('background-color', simColors_sRGB[2]);
+  } else {
+    $('#h11').text(rotColors_sRGB[0]);
+    $('#h12').text(rotColors_sRGB[1]);
+    $('#h13').text(rotColors_sRGB[2]);
+    $('#n11').text(sRGB2Name(rotColors_sRGB[0]));
+    $('#n12').text(sRGB2Name(rotColors_sRGB[1]));
+    $('#n13').text(sRGB2Name(rotColors_sRGB[2]));
+    $('#s11').css('background-color', rotColors_sRGB[0]);
+    $('#s12').css('background-color', rotColors_sRGB[1]);
+    $('#s13').css('background-color', rotColors_sRGB[2]);
+  }
 }
 
 plotRGB('rgbDiv');
@@ -280,14 +380,40 @@ function registerSlider(id) {
 
 registerSlider('#customRange');
 
+function registerSimMode() {
+  $('input[type=radio][name=sim]').change(function() {
+    if (this.id == 'yes') {
+      sim = true;
+      $('#b11').prop('disabled', true);
+      $('#b12').prop('disabled', true);
+      $('#b13').prop('disabled', true);
+      $('#submit').prop('disabled', true);
+    } else {
+      sim = false;
+      $('#b11').prop('disabled', false);
+      $('#b12').prop('disabled', false);
+      $('#b13').prop('disabled', false);
+      $('#submit').prop('disabled', false);
+    }
+  });
+}
+
+registerSimMode();
+
 function registerPickType() {
   $('input[type=radio][name=pick]').change(function() {
     if (this.id == 'pickp') {
-      norm = p_norm;
+      type = 0;
+      line = p_line;
+      norm = p_norm1;
     } else if (this.id == 'pickd') {
-      norm = d_norm;
+      type = 1;
+      line = d_line;
+      norm = d_norm1;
     } else if (this.id == 'pickt') {
-      norm = t_norm;
+      type = 2;
+      line = t_line;
+      norm = t_norm1;
     }
   });
 }
@@ -314,7 +440,7 @@ function registerSetSecondary(buttonId, baseId, textId, squareId, colorId, nameI
     var baseColor = sRGB2RGB(rgb2hex($(baseId).css('background-color')));
     var scale = $(textId).val();
 
-    var val = RGB2sRGB([baseColor[0] + norm[0] * scale, baseColor[1] + norm[1] * scale, baseColor[2] + norm[2] * scale]);
+    var val = RGB2sRGB([baseColor[0] + line[0] * scale, baseColor[1] + line[1] * scale, baseColor[2] + line[2] * scale]);
     $(squareId).css('background-color', val);
     $(colorId).text(val);
     $(nameId).text(sRGB2Name(val));
@@ -324,8 +450,8 @@ function registerSetSecondary(buttonId, baseId, textId, squareId, colorId, nameI
 registerSetSecondary('#b12', '#s11', '#t12', '#s12', '#h12', '#n12');
 registerSetSecondary('#b13', '#s11', '#t13', '#s13', '#h13', '#n13');
 
-$('#t12').val('0.7');
-$('#t13').val('0.2');
+$('#t12').val('0.2');
+$('#t13').val('0.4');
 $('#b12').trigger('click');
 $('#b13').trigger('click');
 
@@ -343,8 +469,6 @@ function registerSubmit(buttonId, rangeId) {
 
 registerSubmit('#submit', '#customRange');
 $('#submit').trigger('click');
-
-
 
 
 
